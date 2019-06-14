00a50fe0a7b5aafcc89ef6ae4c1b9a52867b66a1
yamauchi5748 <47198760+yamauchi5748@users.noreply.github.com>
2019-06-14 09:07:32 +0900

    #406_[feature]_informations_contents_migration

--- workspace/database/migrations/2019_06_13_230811_create_informations_table.php
+++ workspace/database/migrations/2019_06_13_230811_create_informations_table.php
@@ -38,6 +38,7 @@ class CreateInformationsTable extends Migration
     public function down()
     {
         Schema::dropIfExists('informations_attends');
+        Schema::dropIfExists('informations_contents');
         Schema::dropIfExists('informations');
     }
 }
---
+++ workspace/database/migrations/2019_06_13_235932_create_informations_contents_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInformationsContentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('informations_contents', function (Blueprint $table) {
+            $table->uuid('information_id');
+            $table->integer('placement_id');
+            $table->text('text')->nullable();
+            $table->string('image_url')->nullable();
+            $table->enum('content_type', ['text', 'image']);
+
+            $table->primary('information_id');
+
+            $table->foreign('information_id')
+            ->references('id')->on('informations')
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('informations_contents');
+    }
+}
