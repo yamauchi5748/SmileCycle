efb0aa125d0ed04eb6d40f1426ecb4f845e29f1e
yamauchi5748 <kbc17a23@stu.kawahara.ac.jp>
2019-08-25 10:32:13 +0900

    [fix]CommonAuthControllerの修正

--- workspace/app/Http/Controllers/API/MemberController.php
+++ workspace/app/Http/Controllers/API/MemberController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\API;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Auth\CommonAuthController;
 
-class MemberController extends CommonController
+class MemberController extends CommonAuthController
 {
     /**
      * Display a listing of the resource.
