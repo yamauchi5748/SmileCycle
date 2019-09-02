215fb5fc19e3077139e0d812e19498320dd12d15
yamauchi5748 <kbc17a23@stu.kawahara.ac.jp>
2019-09-02 12:00:49 +0900

    [fix]#421_php_test

--- workspace/database/seeds/MembersSeeder.php
+++ workspace/database/seeds/MembersSeeder.php
@@ -1,8 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use App\Member;
-use App\Company;
+use App\Models\Member;
+use App\Models\Company;
 
 class MembersSeeder extends Seeder
 {
@@ -14,8 +14,18 @@ class MembersSeeder extends Seeder
     public function run()
     {
         $faker = Faker\Factory::create('ja_JP');
+
+        $_id = (string) Str::uuid();
+        $company_id = $faker->randomElement(Company::select()->get())->_id;
+        $company = Company::where('_id', $company_id)->first();
+
+        $members = $company->members;
+        array_push($members, $_id);
+        $company->members = $members;
+        $company->save();
+    
         Member::create([
-            'id' => (string) Str::uuid(),
+            'id' => $_id,
             'api_token' => Str::random(60),
             'is_notification' => true,
             'notification_interval' => '0.5h',
@@ -23,12 +33,13 @@ class MembersSeeder extends Seeder
             'name' => 'admin',
             'ruby' => 'あどみん',
             'post' => '管理者',
-            'tel' => $faker->phoneNumber,
-            'company_id' => $faker->randomElement(Company::select('id')->get()),
+            'telephone_number' => $faker->phoneNumber,
+            'company_id' => $company_id,
             'department_name' => $faker->randomElement(['東京笑門会', '鎌倉笑門会', '大阪笑門会', '愛媛笑門会']),
+            'stamp_groups' => [(string) Str::uuid(), (string) Str::uuid()],
             'mail' => $faker->safeEmail,
             'password' => Hash::make('admin')
         ]);
-        factory(App\Member::class, 100)->create();
+        factory(App\Models\Member::class, 100)->create();
     }
 }
