94c4a371c0ced25b29fdc8fa29730bcc922ca7ff
yamauchi5748 <kbc17a23@stu.kawahara.ac.jp>
2019-09-11 10:11:40 +0900

    [feature]_#431_掲示板の投稿処理作成

--- workspace/app/Http/Controllers/API/ForumController.php
+++ workspace/app/Http/Controllers/API/ForumController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Auth\AuthController;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
+use App\Models\Member;
+use App\Models\Forum;
 
 class ForumController extends AuthController
 {
@@ -25,7 +30,69 @@ class ForumController extends AuthController
      */
     public function store(Request $request)
     {
-        return [ "response" => "return forums.store"];
+        /** 掲示板を登録 **/
+        $now  = (string) Carbon::now('Asia/Tokyo'); // 現在時刻
+
+        /* 掲示板のモデル */
+        $forum = [
+            '_id' => (string) Str::uuid(),          // 掲示板のid
+            'sender_id' => '',                 // 掲示板の投稿者id
+            'sender_name' => '',               // 掲示板の投稿者名
+            'title' => $request->title,             // 掲示板のタイトル
+            'text' => $request->text,               // 掲示板のテキスト
+            'images' => [],                         // 掲示板の画像
+            'comments' => [],                       // 掲示板のコメント
+            'created_at' => $now                    // 掲示板投稿日
+        ];
+
+        /* 画像コンテンツの処理 */
+        if ($request->images) {
+            foreach ($request->images as $image) {
+                /* 画像のuuidを生成 */
+                $image_id = (string) Str::uuid();
+    
+                /* 画像を保存 */
+                Storage::putFileAs('public/images/forums', $image, $image_id . '.png', 'private');
+            
+                /* モデルに画像のidを追加 */
+                $forum['images'][] = $image_id;
+            }
+        }
+
+        /* 投稿者名を取得 */
+        $sender_name_corsor = Member::raw()->aggregate([
+            [
+                '$match' => [
+                    '_id' => $request->post_member_id
+                ]
+            ],
+            [
+                '$project' => [
+                    '_id' => 0,
+                    'name' => 1
+                ]
+            ]
+        ])->toArray();
+
+        /* 投稿者名のデータを整形し、モデルにセット */
+        $forum['sender_name'] = head($sender_name_corsor)->name;
+
+        /* モデルをDBに登録 */
+        Forum::raw()->insertOne($forum);
+
+        /* 返すレスポンスデータを整形 */
+        if($forum){
+            $this->response['forum'] = $forum;
+        }else{
+            $this->response['result'] = false;
+        }
+
+        return response()->json(
+            $this->response,
+            200,
+            [],
+            JSON_UNESCAPED_UNICODE
+        );
     }
 
     /**
