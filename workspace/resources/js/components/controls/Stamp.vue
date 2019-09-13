5e9fb3639fbb62ff302ff7f4534a821f37ccde2d
yamauchi5748 <kbc17a23@stu.kawahara.ac.jp>
2019-09-13 16:52:26 +0900

    [feature]_#433_作成ページの枠組みとルーティング処理実装

--- workspace/resources/js/components/controls/Stamp.vue
+++ workspace/resources/js/components/controls/Stamp.vue
@@ -2,29 +2,7 @@
     <section class="view">
         <div class="layout-flex --align-items-center">
             <h2 class="item-count">12種</h2>
-            <button @click="openDialog" class="normal-button margin-left-auto">スタンプを登録する</button>
-            <div class="mask" :class="{open:isDialogOpen}">
-                <div class="dialog">
-                    <div class="dialog-header">
-                        <h2 class="dialog-title">スタンプ登録</h2>
-                        <button @click="closeDialog" class="dialog-hide-button"></button>
-                    </div>
-                    <div class="dialog-body">
-                        <input class="input" placeholder="会員名" />
-                        <input class="input" placeholder="ふりがな" />
-                        <input class="input" placeholder="電話番号" />
-                        <input class="input" placeholder="メールアドレス" />
-                        <input class="input" placeholder="部門" />
-                        <input class="input" placeholder="会社" />
-                        <input class="input" placeholder="役職名" />
-                        <input class="input" placeholder="パスワード" />
-                    </div>
-                    <div class="dialog-footer">
-                        <button @click="closeDialog" class="flat-button">取り消し</button>
-                        <button class="normal-button">登録する</button>
-                    </div>
-                </div>
-            </div>
+            <router-link class="normal-button margin-left-auto" :to="{name:'controls-stamp-create'}">スタンプを登録する</router-link>
         </div>
         <ul
             class="debug p-layout-stamp-tab-list layout-grid --justify-content-space-between margin-top-big"
@@ -48,12 +26,6 @@ export default {
         };
     },
     methods: {
-        closeDialog() {
-            this.isDialogOpen = false;
-        },
-        openDialog() {
-            this.isDialogOpen = true;
-        },
         handleDragOver(event) {
             event.stopPropagation();
             event.preventDefault();
