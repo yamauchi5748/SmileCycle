35156aa9e2f38da284879215e12394f4faf62e88
yamauchi5748 <kbc17a23@stu.kawahara.ac.jp>
2019-09-20 11:43:38 +0900

    [feature]_#437_チャットルームから会員を退出させる処理

--- workspace/app/Http/Controllers/API/ChatRoomMemberController.php
+++ workspace/app/Http/Controllers/API/ChatRoomMemberController.php
@@ -137,6 +137,80 @@ class ChatRoomMemberController extends AuthController
      */
     public function destroies(Request $request, $chat_room_id)
     {
-        return [ "response" => "return chat_room.members.destroies"];
+        /** 会員をルームから退出させる **/
+        /* 会員の情報を取得 */
+        $members = Member::raw()->aggregate([
+            [
+                '$match' => [
+                    '_id' => [
+                        '$in' => $request->delete_members
+                    ]
+                ]
+            ],
+            [
+                '$project' => [
+                    '_id' => 1,
+                    'name' => 1
+                ]
+            ]
+        ])->toArray();
+
+        /* ルーム更新 */
+        ChatRoom::raw()->updateOne(
+            [
+                '_id' => $chat_room_id,
+                /* ルームの管理者認証 */
+                'admin_member_id' => $this->author->_id,
+                'admin_member_id' => [
+                    '$not' => [
+                        '$in' => $request->delete_members
+                    ]
+                ]
+            ],
+            [
+                '$pullAll' => [
+                    'members' => $members
+                ]
+            ]
+        );
+        
+        /* 更新したルーム情報を取得 */
+        $room_corsor = ChatRoom::raw()->aggregate([
+            /* ルームを指定 */
+            [
+                '$match' => [
+                    '_id' => $chat_room_id,
+                    /* 管理者認証、管理者が削除されてないか */
+                    'admin_member_id' => $this->author->_id,
+                    'admin_member_id' => [
+                        '$not' => [
+                            '$in' => $request->delete_members
+                        ]
+                    ]
+                ]
+            ],
+            [
+                '$project' => [
+                    '_id' => 1,
+                    'members' => 1,
+
+                ]
+            ]
+        ])->toArray();
+
+        /* 返すレスポンスデータを整形 */
+        $room = head($room_corsor);
+        if($room){
+            $this->response['room'] = $room;
+        }else{
+            $this->response['result'] = false;
+        }
+
+        return response()->json(
+            $this->response,
+            200,
+            [],
+            JSON_UNESCAPED_UNICODE
+        );
     }
 }
