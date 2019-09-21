4e98deb402055cbb27f77e3fe9bb6ce27c6c3335
yamauchi5748 <kbc17a23@stu.kawahara.ac.jp>
2019-09-21 21:35:35 +0900

    [fix]_#437_ルーム会員追加APIのバリデーション修正

--- workspace/app/Http/Requests/ChatRoomMemberPost.php
+++ workspace/app/Http/Requests/ChatRoomMemberPost.php
@@ -21,18 +21,37 @@ class ChatRoomMemberPost extends FormRequest
     }
 
     /**
+     * ルート引数は対象にならないのでマージする
+     * @return 配列
+     */
+    protected function validationData()
+    {
+        $request = request();
+        $this->room = ChatRoom::where('_id', $request->chat_room_id)->first();
+        
+        if(!$this->room){
+            /* roomにエラーデータをセット */
+            $this->room = [
+                '_id' => 'uuid'
+            ];
+        }
+
+        return array_merge($this->request->all(), [
+            'chat_room_id' => $this->chat_room_id
+        ]);
+    }
+
+    /**
      * Get the validation rules that apply to the request.
      *
      * @return array
      */
     public function rules()
     {
-        $request = request();
-        $room = ChatRoom::where('_id', $request->chat_room_id)->first();
-        
         return [
+            'chat_room_id' => ['required', 'uuid', 'exists:chat_rooms,_id'],
             'add_members' => ['required', 'array'],
-            'add_members.*' => ['uuid', 'exists:members,_id', Rule::unique('chat_rooms', 'members._id')->where('_id', $room->_id)],
+            'add_members.*' => ['uuid', 'exists:members,_id', Rule::unique('chat_rooms', 'members._id')->where('_id', $this->room['_id'])],
         ];
     }
 
