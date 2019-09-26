3c8c658a6412c54680ff985a7192532d9aff1b5a
Ishikawa <kbc16a01@stu.kawahara.ac.jp>
2019-09-27 08:35:46 +0900

    #430_[add]_コンテンツ送信機能

--- workspace/resources/js/components/chats/Chat.vue
+++ workspace/resources/js/components/chats/Chat.vue
@@ -49,6 +49,7 @@ export default {
 </script>
 <style lang="scss">
 .chat {
+    overflow: hidden;
 }
 
 .controls-body {
--- workspace/resources/js/components/chats/RoomDetails.vue
+++ workspace/resources/js/components/chats/RoomDetails.vue
@@ -5,14 +5,28 @@
         </div>
         <div class="contents">
             <div class="history">
-                <div class="content">
-                    <div class="profile"><img src="" alt="profile"></div>
-                    <div class="message">
-                        <span>myName</span>
-                        <span>2019/10/10 01:10</span>
-                        <p>aaa</p>
-                    </div>
+                <ol>
+                    <li>
+                        <div class="content">
+                            <div class="profile">
+                                <img src="" alt="profile">
+                            </div>
+                            <div class="signature">
+                                <span>myName</span>
+                                <span>2019/10/10 01:10</span>
+                            </div>
+                            <div class="message">aaa</div>
+                        </div>
+                    </li>
+                </ol>
+            </div>
+            <div class="send-content">
+                <div class="input-box">
+                    <img src="" alt="動画像">
+                    <textarea></textarea>
+                    <img src="" alt="stamp" class="stamp">
                 </div>
+                <button class="normal-button">send</button>
             </div>
         </div>
     </div>
@@ -22,9 +36,8 @@ export default {
     props: ["showDetais"]
 }
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
 .details {
-    display: flex;
     flex-grow: 1;
     flex-direction: column;
     background-color: $base-color;
@@ -36,7 +49,6 @@ export default {
 
     .name {
         border-bottom: 1px solid #707070;
-        display: flex;
         height: 48px;
         padding: 0 16px;
         justify-content: flex-start;
@@ -50,20 +62,77 @@ export default {
     .contents {
         margin: 0 auto;
         width: 93%;
-        display: flex;
+        height: 100%;
+        flex-direction: column;
+        position: relative;
         .history {
-            display: flex;
             width: 100%;
-            .content {
-                display: flex;
-                justify-content: flex-start;
-                background-color: #F2F2F2;
-                .profile {
-                    background-color: red;
+            height: 60%;
+            overflow: scroll;
+            ol {
+                width: 100%;
+                li {
+                    width: 100%
                 }
             }
         }
     }
 
+}
+.content {
+    display: grid;
+    grid-template-columns: 1fr 9fr;
+    grid-template-rows: auto auto;
+    width: 100%;
+    background-color: #F2F2F2;
+    .profile {
+        grid-column: 1/1;
+        grid-row: 1/1;
+        margin:  0px;
+        pading: 0px;
+        background-color: red;
+    }
+    .signature {
+        pading: 0px;
+        margin: 0px;
+        grid-column: 2/2;
+        grid-row: 1/1;
+    }
+    .message {
+       pading: 0px;
+       margin: 0px;
+       grid-column: 2/2;
+       grid-row: 2/2
+    }
+}
+.send-content {
+    background-color: blue;
+    position: absolute;
+    top: 64%;
+    height: 64px;
+    width: 100%;
+    display: flex;
+    .input-box {
+        display: flex;
+        flex-grow: 9;
+        margin: 0px;
+        img {
+            flex-grow: 1;
+        }
+        textarea {
+            box-sizing: border-box;
+            height: 100%;
+            flex-grow: 8;
+            font-size: 30px;;
+        }
+        .stamp {
+            flex-grow: 1;
+        }
+        background-color: red;
+    }
+    .normal-button {
+        flex-grow: 1;
+        height: 100%;
+    }
 }
 </style>
