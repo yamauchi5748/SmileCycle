2317a2b3239dd63c7eb91a889f4e3f8a357b3444
kamijiseiya <kbc16a04@stu.kawahara.ac.jp>
2019-09-26 11:40:32 +0900

    #424_featire_members cssと入れ子の細かい修正

--- workspace/resources/js/components/members/members.vue
+++ workspace/resources/js/components/members/members.vue
@@ -4,27 +4,14 @@
   <div id="header">
   </div>
   <div class="members">
-    <h1 class="member_deploy_name"></h1>
-      <div class="member_user_icon"></div>
-        <img src="">
-      <li class=member_"user_name"></li>
-      <li class="member_user_post"></li>
-      <l1 class="member_user_company"></l1>
+    <h1 class="member_deploy_name">愛媛笑門会</h1>
+      <div class="member_user_icon">sample</div>
+      <p class="member_user_name">小川知也</p>
+      <p class="member_user_post">株式会社デイアイシステム</p>
+      <p class="member_user_company">平社員</p>
   </div>
 </body>
 </template>
-
-<script>
-  var demo = new user_date({
-    el: '#demo',
-    user_date:{
-      items:[
-        {deploy_name:'愛媛笑門会', user_name: '小川知也',
-         user_post: '平社員', user_company:'株式会社デイアイシステム'}
-      ]
-    }
-  })
-</script>
 <style lang="scss">
 .members {
     width: 1004px;
@@ -35,68 +22,77 @@
     margin-left: 7em;
     box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
     background-color: #ffffff;
+  }
+  .member_deploy_name{
+    margin-left : 24px ;
+    font-family: HiraKakuProN-W3;
+    font-size: 60px;
+    font-weight: normal;
+    font-style: normal;
+    font-stretch: normal;
+    line-height: 1.9;
+    letter-spacing: normal;
+    text-align: left;
+    color: #f57d00;
+    position: relative;
+    overflow: hidden;
+    padding: .9em;
+  }
+  .member_deploy_name::before,
+  .member_deploy_name::after{
+    content: "";
+    position: absolute;
+    bottom: 0;
+  }
+  .member_deploy_name:before{
+    margin-left : -80px ;
+    border-bottom: 1px solid #707070;
+    width: 100%;
+  }
+  .member_user_icon {
+    margin-top : 60px ;
+    margin-left : 11px ;
+  }
 
-    .members_deploy_name{
-      .members_deploy_name::before,
-      .members_deploy_name::after{
-        content: "";
-        position: absolute;
-        bottom: 0;
-      }
-    }
-    .member_deploy_name:before{
-        width: 1004px;
-        height: 4px;
-        margin-top : 120px ;
-        margin-left : -100px ;
-        border-bottom: 1px solid #707070;
-        width: 100%;
-    }
-    .member_user_icon{
-      margin-top : 60px ;
-      margin-left : 11px ;
-    }
-    .member_user_name{
-        height: 40px;
-        margin-top : -140px ;
-        margin-left : 170px ;
-        vertical-align: middle;
-        font-family: Roboto;
-        font-size: 22px;
-        font-weight: normal;
-        font-style: normal;
-        font-stretch: normal;
-        line-height: 1.32;
-        letter-spacing: normal;
-        text-align: left;
-        color: #000000;
-    }
-    .member_user_company{
-        width: 256px;
-        height: 20px;
-        margin-top : 20px ;
-        margin-left : 180px ;
-        font-family: Roboto;
-        font-size: 20px;
-        font-weight: normal;
-        font-style: normal;
-        font-stretch: normal;
-        line-height: 1.7;
-        letter-spacing: normal;
-        text-align: left;
-        color: rgba(149, 149, 149, 0.87);
-    }
-    .member_user_post{
-        margin-left : 180px ;
-        font-family: Roboto;
-        font-size: 20px;
-        font-weight: normal;
-        font-style: normal;
-        font-stretch: normal;
-        line-height: 1.3;
-        letter-spacing: normal;
-        text-align: left;
-        color: rgba(149, 149, 149, 0.87);
-    }
+  .member_user_name{
+    height: 50px;
+    margin-top : -50px ;
+    margin-left : 170px ;
+    vertical-align: middle;
+    font-family: Roboto;
+    font-size: 22px;
+    font-weight: normal;
+    font-style: normal;
+    font-stretch: normal;
+    line-height: 1.32;
+    letter-spacing: normal;
+    text-align: left;
+    color: #000000;
+  }
+
+  .member_user_post{
+    margin-left : 180px ;
+    font-family: Roboto;
+    font-size: 20px;
+    font-weight: normal;
+    font-style: normal;
+    font-stretch: normal;
+    line-height: 1.3;
+    letter-spacing: normal;
+    text-align: left;
+    color: rgba(149, 149, 149, 0.87);
+  }
+  .member_user_company{
+    margin-top : 20px ;
+    margin-left : 180px ;
+    font-family: HiraKakuProN-W3;
+    font-size: 20px;
+    font-weight: normal;
+    font-style: normal;
+    font-stretch: normal;
+    line-height: 1.7;
+    letter-spacing: normal;
+    text-align: left;
+    color: rgba(149, 149, 149, 0.87);
   }
 </style>
