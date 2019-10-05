3517862da14b1f325ffe096013cb02d93f911c92
cookieItF <cookie.it.f@gmail.com>
2019-10-05 19:00:41 +0900

    #452_[feature]_会員作成のAPI組み込み

--- workspace/resources/js/components/controls/MemberCreate.vue
+++ workspace/resources/js/components/controls/MemberCreate.vue
@@ -3,38 +3,46 @@
         <template #title>会員作成</template>
         <template #body>
             <div class="input-wrapper">
-                <v-input v-model="name" counter :max="15">会員名</v-input>
+                <v-input v-model="property.name" counter :max="15">会員名</v-input>
             </div>
             <div class="input-wrapper">
-                <v-input v-model="ruby" counter :max="30">ふりがな</v-input>
+                <v-input v-model="property.ruby" counter :max="30">ふりがな</v-input>
             </div>
             <div class="input-wrapper">
-                <v-input v-model="phone_number" counter>電話番号</v-input>
+                <v-input v-model="property.phone_number" counter>電話番号</v-input>
             </div>
             <div class="input-wrapper">
-                <v-input v-model="mail">メールアドレス</v-input>
+                <v-input v-model="property.mail">メールアドレス</v-input>
             </div>
             <div class="input-wrapper">
-                <v-input v-model="company_id" counter :max="140">会社名</v-input>
+                <v-input v-model="property.company_id" counter :max="140">会社名</v-input>
             </div>
             <div class="input-wrapper">
-                <v-input v-model="post" counter :max="50">役職</v-input>
+                <v-input v-model="property.post" counter :max="50">役職</v-input>
             </div>
             <div class="input-wrapper">
-                <v-input v-model="department_name" counter>部門</v-input>
+                <v-input v-model="property.department_name" counter>部門</v-input>
             </div>
             <div class="input-wrapper">
-                <v-input v-model="secretary_name">秘書名</v-input>
+                <v-input v-model="property.secretary_name">秘書名</v-input>
             </div>
             <div class="input-wrapper">
-                <v-input v-model="secretary_mail">秘書メールアドレス</v-input>
+                <v-input v-model="property.secretary_mail">秘書メールアドレス</v-input>
             </div>
             <div class="input-wrapper">
-                <v-input v-model="password" type="password" counter :max="100">パスワード</v-input>
+                <v-input v-model="property.password" type="password" counter :max="100">パスワード</v-input>
+            </div>
+            <div class="input-wrapper">
+                <v-input
+                    v-model="property.password_confirmation"
+                    type="password"
+                    counter
+                    :max="100"
+                >パスワード再入力</v-input>
             </div>
             <div class="buttons-wrapper">
-                <button class="flat-button">取り消し</button>
-                <button class="normal-button">登録する</button>
+                <button class="flat-button" @click="$router.go(-1)">取り消し</button>
+                <button class="normal-button" @click="handleClick">登録する</button>
             </div>
         </template>
     </secondary-view>
@@ -46,18 +54,27 @@ import VInput from "../VInput";
 export default {
     data: function() {
         return {
-            profile_image: null,
-            name: "",
-            ruby: "",
-            phone_number: "",
-            mail: "",
-            company_id: "",
-            post: "",
-            department_name: "",
-            secretary_name: "",
-            secretary_mail: "",
-            password: ""
+            property: {
+                name: "",
+                ruby: "",
+                post: "",
+                mail: "",
+                phone_number: "",
+                company_id: "",
+                department_name: "",
+                secretary_name: "",
+                secretary_mail: "",
+                password: "",
+                password_confirmation: ""
+            }
         };
+    },
+    methods: {
+        handleClick: function() {
+            this.$root.createMember(this.property).catch(function(error) {
+                console.error(error);
+            });
+        }
     },
     components: {
         VInput,
