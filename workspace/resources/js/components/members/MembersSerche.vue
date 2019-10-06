12b67df207070992bda72e5d3de53acad3fdb4b2
kamijiseiya <kbc16a04@stu.kawahara.ac.jp>
2019-10-07 01:42:54 +0900

    [add]/#454_検索フォームの処理の追加およびスタイルの追加

---
+++ workspace/resources/js/components/members/MembersSerche.vue
@@ -0,0 +1,32 @@
+<template>
+ <input
+    class="search_text"
+    type="type"
+    name="name"
+    placeholder="検索する"
+    @input="updateValue"
+  />
+</template>
+
+<script>
+export default {
+  name: "MyInput",
+  props: {
+    name: { type: String, require: true },
+    placeholder: { type: String, require: false }
+  },
+  methods: {
+    updateValue: function(e) {
+      this.$emit("input", e.target.value);
+    }
+  }
+};
+</script>
+<style lang="scss">
+.search_text {
+    width: 402px;
+    height: 48px;
+    text-align: center;
+    box-shadow: 0 8px 8px 0 rgba(0, 0, 0, 0.24);
+}
+</style>
