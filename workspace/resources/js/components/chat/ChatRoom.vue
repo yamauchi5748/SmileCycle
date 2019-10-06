16f89b67e5649a57765649f79c22eb369377e2e0
yamauchi5748 <kbc17a23@stu.kawahara.ac.jp>
2019-10-07 01:07:03 +0900

    [update]_#430_コンポーネントの結合

--- workspace/resources/js/components/chat/ChatRoom.vue
+++ workspace/resources/js/components/chat/ChatRoom.vue
@@ -1,10 +1,287 @@
 <template>
-  <div>
-chat-room
+  <div class="details">
+    <div class="name">
+      <span v-if="room">{{ room.group_name }}</span>
+    </div>
+    <div class="contents">
+      <div class="history">
+        <ol>
+          <li v-for="(content, index) in contents" :key="index">
+            <div class="content">
+              <div class="profile">
+                <img src alt="profile" />
+              </div>
+              <div class="signature">
+                <span>{{ content.sender_name }}</span>
+                <span>{{ content.created_at }}</span>
+              </div>
+              <div class="message">{{ content.content_id }}</div>
+            </div>
+          </li>
+        </ol>
+      </div>
+      <div class="send-content">
+        <div class="input-box">
+          <img src alt />
+          <textarea v-on:input="test"></textarea>
+          <img src alt="stamp" class="stamp" />
+        </div>
+        <button class="normal-button">send</button>
+      </div>
+    </div>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      auth: true,
+      result: true,
+      is_group: true,
+      contents: [
+        //最大10件まで取得
+        {
+          _id: "68b02ded-fb25-4154-abdc-1f78a3b0ce49",
+          sender_id: "ae96095c-dc5b-4fa3-83b6-4ccec79a7f94",
+          sender_name: "bear",
+          content_type: "4", //1:テキスト, 2:スタンプ, 3:画像, 4:動画
+          content_id: "372b0690-d917-46f7-8fce-2f87d14e4bbc",
+          created_at: "2019/09/18 10:40",
+          already_read: 0
+        },
+        {
+          _id: "68b02ded-fb25-4154-abdc-1f78a3b0ce49",
+          sender_id: "ae96095c-dc5b-4fa3-83b6-4ccec79a7f94",
+          sender_name: "bear",
+          content_type: "4", //1:テキスト, 2:スタンプ, 3:画像, 4:動画
+          content_id: "372b0690-d917-46f7-8fce-2f87d14e4bbc",
+          created_at: "2019/09/20 10:50",
+          already_read: 0
+        },
+        {
+          _id: "68b02ded-fb25-4154-abdc-1f78a3b0ce49",
+          sender_id: "ae96095c-dc5b-4fa3-83b6-4ccec79a7f94",
+          sender_name: "bear",
+          content_type: "4", //1:テキスト, 2:スタンプ, 3:画像, 4:動画
+          content_id: "372b0690-d917-46f7-8fce-2f87d14e4bbc",
+          created_at: "2019/09/18 10:40",
+          already_read: 0
+        },
+        {
+          _id: "68b02ded-fb25-4154-abdc-1f78a3b0ce49",
+          sender_id: "ae96095c-dc5b-4fa3-83b6-4ccec79a7f94",
+          sender_name: "bear",
+          content_type: "4", //1:テキスト, 2:スタンプ, 3:画像, 4:動画
+          content_id: "372b0690-d917-46f7-8fce-2f87d14e4bbc",
+          created_at: "2019/09/20 10:50",
+          already_read: 0
+        },
+        {
+          _id: "68b02ded-fb25-4154-abdc-1f78a3b0ce49",
+          sender_id: "ae96095c-dc5b-4fa3-83b6-4ccec79a7f94",
+          sender_name: "bear",
+          content_type: "4", //1:テキスト, 2:スタンプ, 3:画像, 4:動画
+          content_id: "372b0690-d917-46f7-8fce-2f87d14e4bbc",
+          created_at: "2019/09/18 10:40",
+          already_read: 0
+        },
+        {
+          _id: "68b02ded-fb25-4154-abdc-1f78a3b0ce49",
+          sender_id: "ae96095c-dc5b-4fa3-83b6-4ccec79a7f94",
+          sender_name: "bear",
+          content_type: "4", //1:テキスト, 2:スタンプ, 3:画像, 4:動画
+          content_id: "372b0690-d917-46f7-8fce-2f87d14e4bbc",
+          created_at: "2019/09/20 10:50",
+          already_read: 0
+        },
+        {
+          _id: "68b02ded-fb25-4154-abdc-1f78a3b0ce49",
+          sender_id: "ae96095c-dc5b-4fa3-83b6-4ccec79a7f94",
+          sender_name: "bear",
+          content_type: "4", //1:テキスト, 2:スタンプ, 3:画像, 4:動画
+          content_id: "372b0690-d917-46f7-8fce-2f87d14e4bbc",
+          created_at: "2019/09/20 10:50",
+          already_read: 0
+        },
+        {
+          _id: "68b02ded-fb25-4154-abdc-1f78a3b0ce49",
+          sender_id: "ae96095c-dc5b-4fa3-83b6-4ccec79a7f94",
+          sender_name: "bear",
+          content_type: "4", //1:テキスト, 2:スタンプ, 3:画像, 4:動画
+          content_id: "372b0690-d917-46f7-8fce-2f87d14e4bbc",
+          created_at: "2019/09/18 10:40",
+          already_read: 0
+        },
+        {
+          _id: "68b02ded-fb25-4154-abdc-1f78a3b0ce49",
+          sender_id: "ae96095c-dc5b-4fa3-83b6-4ccec79a7f94",
+          sender_name: "bear",
+          content_type: "4", //1:テキスト, 2:スタンプ, 3:画像, 4:動画
+          content_id: "372b0690-d917-46f7-8fce-2f87d14e4bbc",
+          created_at: "2019/09/20 10:50",
+          already_read: 0
+        },
+        {
+          _id: "68b02ded-fb25-4154-abdc-1f78a3b0ce49",
+          sender_id: "ae96095c-dc5b-4fa3-83b6-4ccec79a7f94",
+          sender_name: "bear",
+          content_type: "4", //1:テキスト, 2:スタンプ, 3:画像, 4:動画
+          content_id: "372b0690-d917-46f7-8fce-2f87d14e4bbc",
+          created_at: "2019/09/20 10:50",
+          already_read: 0
+        },
+        {
+          _id: "68b02ded-fb25-4154-abdc-1f78a3b0ce49",
+          sender_id: "ae96095c-dc5b-4fa3-83b6-4ccec79a7f94",
+          sender_name: "bear",
+          content_type: "4", //1:テキスト, 2:スタンプ, 3:画像, 4:動画
+          content_id: "372b0690-d917-46f7-8fce-2f87d14e4bbc",
+          created_at: "2019/09/18 10:40",
+          already_read: 0
+        },
+        {
+          _id: "68b02ded-fb25-4154-abdc-1f78a3b0ce49",
+          sender_id: "ae96095c-dc5b-4fa3-83b6-4ccec79a7f94",
+          sender_name: "bear",
+          content_type: "4", //1:テキスト, 2:スタンプ, 3:画像, 4:動画
+          content_id: "372b0690-d917-46f7-8fce-2f87d14e4bbc",
+          created_at: "2019/09/20 10:50",
+          already_read: 0
+        }
+      ]
+    };
+  },
+
+  computed: {
+    room: function() {
+      return this.$root.chat_room_list.filter(room => {
+        return room._id === this.$route.params.id;
+      })[0];
+    }
+  },
+
+  methods: {
+    test(e) {
+      console.log("hello: " + e.target.value.length);
+      console.log(e.target.clientHeight);
+      console.log(e.target.span);
+      console.log(e.target);
+      if (e.target.value.length == 33) {
+        e.target.style.color = "red";
+      }
+    }
+  }
+};
 </script>
 <style lang="scss" scoped>
+.details {
+  flex-grow: 1;
+  flex-direction: column;
+  background-color: $base-color;
+  height: 800px;
+  div {
+    margin: 0px;
+    margin-top: 20px;
+  }
+
+  .name {
+    border-bottom: 1px solid #707070;
+    height: 48px;
+    padding: 0 16px;
+    justify-content: flex-start;
+    align-items: center;
+    text-decoration: none;
+    font-size: 16px;
+    font-weight: bold;
+    color: $black;
+  }
+
+  .contents {
+    margin: 0 auto;
+    width: 93%;
+    height: 100%;
+    flex-direction: column;
+    position: relative;
+    overflow-y: hidden;
+    overflow-x: hidden;
+    .history {
+      width: calc(100% + 17px);
+      height: 60%;
+      overflow-y: scroll;
+      ol {
+        width: 100%;
+        li {
+          width: 100%;
+        }
+      }
+    }
+  }
+}
+.content {
+  display: grid;
+  grid-template-columns: 1fr 9fr;
+  grid-template-rows: auto auto;
+  width: 100%;
+  background-color: #f2f2f2;
+  .profile {
+    grid-column: 1/1;
+    grid-row: 1/1;
+    margin: 0px;
+    padding: 0px;
+    background-color: red;
+  }
+  .signature {
+    padding: 0px;
+    margin: 0px;
+    grid-column: 2/2;
+    grid-row: 1/1;
+  }
+  .message {
+    padding: 0px;
+    margin: 0px;
+    grid-column: 2/2;
+    grid-row: 2/2;
+  }
+}
+.send-content {
+  background-color: blue;
+  position: absolute;
+  top: 64%;
+  height: 64px;
+  width: 100%;
+  display: flex;
+  .input-box {
+    display: flex;
+    flex-grow: 9;
+    margin: 0px;
+    img {
+      flex-grow: 1;
+    }
+    textarea {
+      box-sizing: border-box;
+      height: 100%;
+      width: calc(100% + 17px);
+      flex-grow: 8;
+      font-size: 30px;
+    }
+    .stamp {
+      flex-grow: 1;
+    }
+    background-color: red;
+  }
+  .normal-button {
+    flex-grow: 1;
+    height: 100%;
+  }
+}
 </style>
+
+
+
+
+
+
+
+
+
+
