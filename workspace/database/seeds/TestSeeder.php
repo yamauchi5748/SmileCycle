331c406bc328461e9ed04ab634ea343df099bf5c
furukawa_twua <kbc17a19@stu.kawahara.ac.jp>
2019-11-04 19:35:37 +0900

    [add]_#460_「GET /」のテストの作成
    TestSeederをテストデータ作成のため追加
    HomeTestを「GET /」のテストのため追加
    RefreshDatabaseをテストデータのリフレッシュのため追加
    TestCaseにsetUpTraits()を追加

---
+++ workspace/database/seeds/TestSeeder.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Member;
+use App\Models\Company;
+
+class TestSeeder extends Seeder
+{
+    /**
+     * Run the test data seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // _id, name, ruby, post, mail, api_token, password, telephone_number, secretary, stamp_groups, department_name, is_notification, notification_interval, is_admin, invitations
+        $members = [
+            ['83b5603f-8436-4c93-189e-8387c15f823f', '橋本環奈', 'はしもとかんな', '代表取締役社長', 'hashikann@hashikann.com', 'EgGvxAr4Wm97jGg1VNitMGj5JFovqhTZEDSIssnmkiFF6Sp0anJ5v07nLeHQ', '$2y$10$MPEgFZgwoRLZ3idAD4Oo9uSO2kGWl33o4QOvVIPU5Cu8z2TXQBMvS'/* 19990203 */, '080-1234-5678', ['name' => '小泉純一郎', 'mail' => 'koizumi@hashikann.com'], [], '東京笑門会', true, '1h', false, []]
+        ];
+
+        // _id, name, address, telephone_number, members
+        $companies = [
+            ['e6a91e70-bae6-08e0-8ee7-0a7f595f30fc', '株式会社橋環', '東京都hoge区', '0120-123-456', ['83b5603f-8436-4c93-189e-8387c15f823f']]
+        ];
+
+        for ($i = 0; $i < count($members); $i++) {
+            Member::create([
+                '_id' => $members[$i][0],
+                'name' => $members[$i][1],
+                'ruby' => $members[$i][2],
+                'post' => $members[$i][3],
+                'mail' => $members[$i][4],
+                'api_token' => $members[$i][5],
+                'password' => $members[$i][6],
+                'telephone_number' => $members[$i][7],
+                'secretary' => $members[$i][8],
+                'stamp_groups' => $members[$i][9],
+                'department_name' => $members[$i][10],
+                'is_notification' => $members[$i][11],
+                'notification_interval' => $members[$i][12],
+                'is_admin' => $members[$i][13],
+                'invitations' => $members[$i][14]
+            ]);
+        }
+
+        for ($i = 0; $i < count($companies); $i++) {
+            Company::create([
+                '_id' => $companies[$i][0],
+                'name' => $companies[$i][1],
+                'address' => $companies[$i][2],
+                'telephone_number' => $companies[$i][3],
+                'members' => $companies[$i][4]
+            ]);
+        }
+    }
+}
--- workspace/tests/TestCase.php
+++ workspace/tests/TestCase.php
@@ -7,4 +7,20 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * Illuminate\Foundation_Testing\TestCaseのsetUpTraitsをオーバーライドし、Tests\RefreshDatabaseトレイトを追加
+     *
+     * @return array
+     */
+    protected function setUpTraits()
+    {
+        $uses = parent::setUpTraits();
+
+        if (isset($uses[RefreshDatabase::class])) {
+            $this->refreshDatabase();
+        }
+
+        return $uses;
+    }
 }
