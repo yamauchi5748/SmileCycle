ba340ac45fda328c8be0612ad1171c60689693b1
seno105 <iyi2adpke@i.softbank.jp>
2020-02-07 14:06:48 +0900

    更新

--- workspace/srv/api/images.js
+++ workspace/srv/api/images.js
@@ -1,8 +1,21 @@
+const { Image } = require("../model");
 const { Router } = require("express");
 const debug = require("debug")("app:api-images");
 const router = Router();
+
 const multer = require("multer");
-const upload = multer({ dest: "images/" });
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, "images/")
+        },
+        filename: async function (req, file, cb) {
+            const image = await Image.create({});
+            debug(image);
+            cb(null, image._id.toString());
+        }
+    })
+});
 const fs = require("fs");
 
 router.get("/:name", async function (req, res, next) {
@@ -12,6 +25,7 @@ router.get("/:name", async function (req, res, next) {
         res.send(data);
     });
 });
+
 router.post("/", upload.single("file"), async function (req, res, next) {
     res.json({ name: req.file.filename });
 });
