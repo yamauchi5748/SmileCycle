4cbe23364e1c6931c9112da5437d3d137c2d082a
furukawa_twua <kbc17a19@stu.kawahara.ac.jp>
2020-02-13 16:56:19 +0900

    update]stamp validation

--- workspace/src/components/MStampEditor.vue
+++ workspace/src/components/MStampEditor.vue
@@ -21,13 +21,14 @@
                         <v-col cols="12" v-show="!editedItem.isAll">
                             <VSelectMembers v-model="editedItem.members"></VSelectMembers>
                         </v-col>
+                        <v-col cols="12" v-show="error" v-text="errorText" class="red--text"></v-col>
                     </v-row>
                 </v-container>
             </v-card-text>
             <v-card-actions>
                 <v-spacer></v-spacer>
                 <v-btn color="accent" text @click="close">取り消し</v-btn>
-                <v-btn :disabled="!valid" color="accent" text @click="save">保存する</v-btn>
+                <v-btn :disabled="error" color="accent" text @click="save">保存する</v-btn>
             </v-card-actions>
         </v-card>
     </v-dialog>
@@ -62,18 +63,31 @@ export default {
             isAll: true,
             members: []
         },
-        stampRules: {
-            tabImage: [v => v || "必須項目です。"],
-            stamps: [
-                v =>
-                    (v && v.length) || "必須項目です。1つ以上選択してください。"
-            ]
-        },
         stamps: []
     }),
     computed: {
         formTitle() {
             return this.editedId ? "スタンプ編集" : "スタンプ作成";
+        },
+        error() {
+            const tabImageIsEmpty = !this.editedItem.tabImage;
+            const stampsIsEmpty = this.editedItem.stamps.length == 0;
+            const notAllAndMembersEmpty = (!this.editedItem.isAll && this.editedItem.members.length == 0);
+            return tabImageIsEmpty || stampsIsEmpty || notAllAndMembersEmpty;
+        },
+        errorText() {
+            const tabImageIsEmpty = !this.editedItem.tabImage;
+            const stampsIsEmpty = this.editedItem.stamps.length == 0;
+            const notAllAndMembersEmpty = (!this.editedItem.isAll && this.editedItem.members.length == 0);
+            if (tabImageIsEmpty) {
+                return "タブ画像を選択してください。";
+            } else if (stampsIsEmpty) {
+                return "スタンプを選択してください。";
+            } else if (notAllAndMembersEmpty) {
+                return "会員を選択してください。";
+            } else {
+                return "";
+            }
         }
     },
     watch: {
